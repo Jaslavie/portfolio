dc012bef9501dfc7d0f76e9484f48c2ccc4112bd
Jasmine <jasmineqw45@gmail.com>
2024-07-06 19:11:16 -0700

    landing page is relatively finished with aesthetic changes left

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../Assets/JasmineWuLogoLong.svg";
-
+import { motion } from "framer-motion";
 function NavBar() {
   const location = useLocation();
   const [activePath, setActivePath] = useState(location.pathname);
@@ -16,38 +16,56 @@ function NavBar() {
   ];
 
   return (
-    <div className="navbar">
-      <ul>
-        <li className={activePath === "/" ? "active" : ""}>
-          <Link to="/" onClick={() => setActivePath("/")}>
-            <img src={Logo} alt="Logo" />
-          </Link>
-        </li>
-        <li className={activePath === "/about" ? "active" : ""}>
-          <Link to="/about" onClick={() => setActivePath("/about")}>
-            About
-          </Link>
-        </li>
-        <li className={activePath === "/play" ? "active" : ""}>
-          <Link to="/play" onClick={() => setActivePath("/play")}>
-            Play
-          </Link>
-        </li>
-        {projects.map((project) => (
-          <li
-            key={project.id}
-            className={activePath === `/project/${project.id}` ? "active" : ""}
-          >
-            <Link
-              to={`/project/${project.id}`}
-              onClick={() => setActivePath(`/project/${project.id}`)}
-            >
-              {project.name}
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.2,
+        duration: 0.5,
+        type: "spring",
+        stiffness: 60,
+        damping: 15,
+      }}
+      className="dashboard-contain-left"
+    >
+      <div className="navbar">
+        <ul>
+          {" "}
+          <li className={activePath === "/" ? "active" : ""}>
+            <Link to="/" onClick={() => setActivePath("/")}>
+              <img src={Logo} alt="Logo" />
             </Link>
           </li>
-        ))}
-      </ul>
-    </div>
+        </ul>
+        <ul>
+          <li className={activePath === "/about" ? "active" : ""}>
+            <Link to="/about" onClick={() => setActivePath("/about")}>
+              About
+            </Link>
+          </li>
+          <li className={activePath === "/play" ? "active" : ""}>
+            <Link to="/play" onClick={() => setActivePath("/play")}>
+              Play
+            </Link>
+          </li>
+          {projects.map((project) => (
+            <li
+              key={project.id}
+              className={
+                activePath === `/project/${project.id}` ? "active" : ""
+              }
+            >
+              <Link
+                to={`/project/${project.id}`}
+                onClick={() => setActivePath(`/project/${project.id}`)}
+              >
+                {project.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </motion.div>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,37 @@
 import NavBar from "../Components/NavBar";
 import Reveal from "../Components/Reveal";
-
+import { TypeAnimation } from "react-type-animation";
 function Home() {
   return (
     <div>
       {" "}
       <NavBar />
       <Reveal width="100%">
-        <h1>Welcome to the Landing Page</h1>
+        <section>
+          <div className="hero">
+            <h1>Jasmine is a</h1>
+
+            <h1>
+              <span>
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    "User-obsessed Engineer",
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    "Data-driven Designer",
+                    1000,
+                    "Hackathon Enthusiast",
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={10}
+                  repeat={Infinity}
+                />
+              </span>
+            </h1>
+            <p>18 y.o. design engineer building @ NASA</p>
+          </div>
+        </section>
       </Reveal>
     </div>
   );
